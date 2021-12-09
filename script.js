0988e620cac946b90c2c14fe7854309b3c362a74
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2021-12-09 13:45:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var cps = 0
 function set_element_id(id,text){
   document.getElementById(id).innerHTML = text
 }
-window.setInterval(function(){
-  cps = 1
-  code = code + cps
-  set_element_id("mon",code)
-},000)
+//window.setInterval(function(){
+  //cps = 1
+  //code = code + cps
+  //set_element_id("mon",code)
+//},1000)
 function click(){
   code = code + 1
   document.getElementById("mon").innerHTML = code 
