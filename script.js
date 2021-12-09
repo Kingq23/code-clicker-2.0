52ab0d31d5351e137e5d0afaf82d21ba38c29c9f
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2021-12-09 12:43:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ function set_element_id(id,text){
 window.setInterval(function(){
   cps = 1
   code = code + cps
-
   set_element_id("mon",code)
 },1000)
 function click(){
   code = code + 1
-  set_element_id("mon",code)
+  document.getElementById("mon").innerHTML = code 
   console.log("e")
 }
